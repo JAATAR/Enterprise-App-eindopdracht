cefa0b825bb40a1f3b2e1e63758a3973e58b4e58
JAATAR <133091960+JAATAR@users.noreply.github.com>
2025-12-08 15:19:56 +0100

    Materiaalverhuur Initial Setup

---
+++ src/main/java/be/kunstacademie/materiaalverhuur/EnterpriseAppEindopdrachtApplication.java
@@ -0,0 +1,13 @@
+package be.kunstacademie.materiaalverhuur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseAppEindopdrachtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterpriseAppEindopdrachtApplication.class, args);
+	}
+
+}
